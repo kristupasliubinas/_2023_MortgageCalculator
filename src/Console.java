907f46e8ca5533931b69418d2e52489ea735a561
kristupas <kristupas.liubinas@gmail.com>
2023-12-08 10:56:44 +0000

    Small optimisations to the Console class

--- src/Console.java
+++ src/Console.java
@@ -1,10 +1,9 @@
 import java.util.Scanner;
 
 public class Console {
-    public static double readInput(String inputMessage, double min, double max) {
+    private static Scanner scanner = new Scanner(System.in);
+    public static double readNumber(String inputMessage, double min, double max) {
         double input;
-        Scanner scanner = new Scanner(System.in);
-
         while (true) {
             System.out.print(inputMessage);
             input = scanner.nextDouble();
@@ -12,7 +11,6 @@ public class Console {
                 break;
             System.out.println("Enter a number between " + min + " and " + max + ".");
         }
-
         return input;
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,9 @@
 public class Main {
     public static void main(String[] args) {
         // Take input from user
-        int principal = (int) Console.readInput("Principal loan (£1K - £1M): ", 1000, 1_000_000);
-        float annualInterestRate = (float) Console.readInput("Annual Interest Rate: ", 1, 30);
-        int years = (int) Console.readInput("Period (Years): ", 1, 30);
+        int principal = (int) Console.readNumber("Principal loan (£1K - £1M): ", 1000, 1_000_000);
+        float annualInterestRate = (float) Console.readNumber("Annual Interest Rate: ", 1, 30);
+        int years = (int) Console.readNumber("Period (Years): ", 1, 30);
 
         var mortgage = new Mortgage(principal, annualInterestRate, years);
         new Report(mortgage);
